1184ca8b9bdff1ae792141ffb89fc3665b3f1224
Raizelian <nesphyd@live.it>
2022-09-19 14:37:25 +0200

    Create square-root.swift

---
+++ square-root.swift
@@ -0,0 +1,41 @@
+enum squareRootError: Error {
+  case noRoot, outOfBounds
+}
+
+func squareRoot (_ number: Int) throws -> Int {
+
+  if number >= 1 && number <= 10000 {
+
+    var temp = 1
+    while temp <= number && (temp * temp) != number {
+//      print("DEBUG : Trying", temp)
+        temp += 1
+        continue
+    }
+
+    let tempRes = temp
+
+    if (tempRes * tempRes) == number {
+      let radix = tempRes
+      return(radix)
+    } else {
+        throw squareRootError.noRoot
+    }
+
+  } else {
+      throw squareRootError.outOfBounds
+    }
+}
+
+var number = 10_000
+
+do {
+  let result = try squareRoot(number)
+  print("The square root of \(number) is", result)
+} catch squareRootError.noRoot {
+  print("No root!")
+} catch squareRootError.outOfBounds {
+  print("Out of bounds!")
+} catch {
+  print("An error occurred.")
+}
